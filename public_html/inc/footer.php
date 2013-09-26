4d3a59388fc6e5cf2d101343551f94665919e8f0
jonathanstark <jstark@jonathanstark.com>
2013-09-26 00:11:49 +0000

    fixed the active/hover highlight on the header link

--- public_html/inc/footer.php
+++ public_html/inc/footer.php
@@ -1,24 +1,6 @@
         <div class="footer">
             <p>Thanks for stopping by! Please feel free to <a rel="external" href="mailto:hello@nitch.cc?subject=Nitch!">say hello</a> :)</p>
         </div>
-        <script type="text/javascript" charset="utf-8">
-            (function (d, c) {
-                var activeClass = c || "active";
-                function clearActive() {
-                    var actives = d.querySelectorAll("." + activeClass);
-                    for (var i = 0; i < actives.length; i++) {
-                        actives[i].classList.remove(activeClass);
-                    }
-                }
-                function setActive(e) {
-                    clearActive();
-                    if (e.target.tagName == "A") {
-                        e.target.classList.add(activeClass);
-                    }
-                }
-                d.body.addEventListener("touchstart", setActive, false);
-                d.body.addEventListener("touchmove" , clearActive, false);
-            })(document);
-        </script>
+        <script src="https://raw.github.com/jonathanstark/fastactive/master/FastActive.js" type="text/javascript" charset="utf-8" async defer></script>
     </body>
 </html>
