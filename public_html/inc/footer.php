90dc93ab46d00e8c3391364febef438879b103b4
jonathanstark <jstark@jonathanstark.com>
2013-10-04 07:02:20 +0000

    inlined fastactive in place of external link

--- public_html/inc/footer.php
+++ public_html/inc/footer.php
@@ -1,6 +1,8 @@
         <div class="footer">
             <p>Thanks for stopping by! Please feel free to <a rel="external" href="mailto:hello@nitch.cc?subject=Nitch!">say hello</a> :)</p>
         </div>
-        <script src="https://raw.github.com/jonathanstark/fastactive/master/FastActive.js" type="text/javascript" charset="utf-8" async defer></script>
+        <script>
+            (function(a,c,d){if("ontouchstart"in c||c.a&&a instanceof DocumentTouch){var b=null,e=function(){b&&(b.classList.remove(d),b=!1)};a.documentElement.classList.add("touch");a.body.addEventListener("touchstart",function(a){e();"A"==a.target.tagName&&(b=a.target,b.classList.add(d))},!1);a.body.addEventListener("touchmove",e,!1)}else a.documentElement.classList.add("no-touch")})(document,window,"active");
+        </script>
     </body>
 </html>
