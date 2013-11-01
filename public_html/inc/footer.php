dc4b8890a605fcf330d82c103182d0d29638c4f9
jonathanstark <jstark@jonathanstark.com>
2013-11-01 18:24:18 +0000

    added tracking and maybe some other stuff

--- public_html/inc/footer.php
+++ public_html/inc/footer.php
@@ -2,7 +2,17 @@
             <p>Thanks for stopping by! Please feel free to <a rel="external" href="mailto:hello@nitch.cc?subject=Nitch!">say hello</a> :)</p>
         </div>
         <script>
-            (function(a,c,d){if("ontouchstart"in c||c.a&&a instanceof DocumentTouch){var b=null,e=function(){b&&(b.classList.remove(d),b=!1)};a.documentElement.classList.add("touch");a.body.addEventListener("touchstart",function(a){e();"A"==a.target.tagName&&(b=a.target,b.classList.add(d))},!1);a.body.addEventListener("touchmove",e,!1)}else a.documentElement.classList.add("no-touch")})(document,window,"active");
+        (function(a,c,d){if("ontouchstart"in c||c.a&&a instanceof DocumentTouch){var b=null,e=function(){b&&(b.classList.remove(d),b=null)};a.documentElement.classList.add("touch");a.documentElement.classList.remove("no-touch");a.body.addEventListener("touchstart",function(a){e();"A"==a.target.tagName&&(b=a.target,b.classList.add(d))},!1);a.body.addEventListener("touchmove",e,!1)}else a.documentElement.className+=" no-touch"})(document,window,"active");
         </script>
+	    <script type="text/javascript">
+	      var _gaq = _gaq || [];
+	      _gaq.push(['_setAccount', 'UA-985459-20']);
+	      _gaq.push(['_trackPageview']);
+	      (function() {
+	        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	      })();
+	    </script>
     </body>
 </html>
