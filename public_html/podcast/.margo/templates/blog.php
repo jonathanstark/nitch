68d7f7e905eb39d8c0cf8ec11dbce17ea87b13ea
Jonathan Stark <jstark@jonathanstark.com>
2013-11-21 18:07:19 +0000

    tweaked site subtitle

--- public_html/podcast/.margo/templates/blog.php
+++ public_html/podcast/.margo/templates/blog.php
@@ -1,5 +1,5 @@
 <?php require_once '../../inc/header.php'; ?>
-	<div class="section podcast index description"><p>A weekly podcast about making apps that run everywhere using open web standards like HTML, CSS, JavaScript, REST, and JSON. Hosted by <a href="http://jonathanstark.com">Jonathan Stark</a> and <a href="http://kellishaver.com">Kelli&nbsp;Shaver</a>.</p></div>
+	<div class="section podcast index description"><p>A weekly podcast about building apps that run everywhere using open web standards like HTML, CSS, JavaScript, REST, and JSON. Hosted by <a href="http://jonathanstark.com">Jonathan Stark</a> and <a href="http://kellishaver.com">Kelli&nbsp;Shaver</a>.</p></div>
     <div class="section podcast index">
         <?php echo $html ?>
     </div>
