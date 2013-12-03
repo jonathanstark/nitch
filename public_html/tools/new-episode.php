6962d33bea068af46be24892c7e8de0c09b1b52e
Jonathan Stark <jstark@jonathanstark.com>
2013-12-03 06:57:44 +0000

    added features to sode o matic (autofill form data, regex scrubbing of input, etc)

--- public_html/tools/new-episode.php
+++ public_html/tools/new-episode.php
@@ -48,14 +48,27 @@ Running time: %3$s
 %9$s
 ';
 #
+# Grab the xml for the itunes rss feed because we need it for a couple things farther down
+$itunes_xml = simplexml_load_file('http://nitch.cc/itunes.rss');
+// print_r($itunes_xml);die;
+#
 # If post is set, render the templates and trigger a download
 if (isset($_POST['action'])) {
     // print_r($_POST);die;
     #
     # Calc some values
     $_POST['display_title'] = sprintf('Episode %s: %s', $_POST['episode_number'], $_POST['episode_name']);
-    $_POST['page_name'] = strtolower(str_replace(' ', '-', str_replace(array(':', '’', '\''), '', $_POST['display_title'])));
-    $_POST['audio_file_name'] = str_replace(' ', '_', str_replace(array(':', '’', '\''), '', $_POST['display_title']));
+    $_POST['audio_file_name'] = str_replace(' ', '_', preg_replace('/[^a-zA-Z0-9 ]+/', '', $_POST['display_title']));
+    $_POST['page_name'] = strtolower(str_replace('_', '-', $_POST['audio_file_name']));
+    // print_r($_POST);die;
+    #
+    # Loop through the itunes rss feed to make sure that this title is unique
+    foreach ($itunes_xml->channel->item as $item) {
+        $episode_name = explode(': ', (string)$item->title)[1];
+        if(strtolower($_POST['episode_name']) == strtolower($episode_name)) {
+            die('You already used ' . $episode_name . ' as a name. Click your back button, dope.');
+        }
+    }
     #
     # Loop through related links and create, well... links
     $links = explode('*', $_POST['links']);
@@ -87,7 +100,9 @@ if (isset($_POST['action'])) {
             $_POST['links'].= sprintf('* [%s](%s "%s")', $link, $url, $title) . PHP_EOL;
         }
     }
-    // print_r($_POST['links']);die;
+    #
+    # Kill the trailing line return
+    $_POST['links'] = trim($_POST['links']);
     #
     # Get ready to poop out some text
     header('Content-Type: text/plain');
@@ -115,26 +130,40 @@ if (isset($_POST['action'])) {
     );
     die;
 }
+#
+# Calc the date of the coming friday (or todays date if it is friday)
+$this_friday = date('Y-m-d', strtotime('+'. (5 - date('N')) . ' days'));
+#
+# Calc the number of the episode based on the rss feed
+$last_title = (string)$itunes_xml->channel->item[count($itunes_xml->channel->item)-1]->title;
+preg_match('/[0-9]+/', $last_title, $matches);
+$this_episode_number = $matches[0] + 1;
+#
 ?>
 <!doctype html>
 <html>
     <head>
+        <link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
         <title>The Nitch Podcast "Sode-O-Matic!"</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style type="text/css" media="screen">
-            *{box-sizing:border-box}html{background:#F4F4F4}body{font-size:large;padding:1em}body,input,textarea{color:#797B7B;font-family:"Helvetica-Neue",Helvetica,sans-serif}input,textarea{width:100%}input[type=submit]{width:auto}.form-item{margin-bottom:1em}.topcoat-text-input,.topcoat-text-input--large{line-height:1.313rem;font-size:12px;letter-spacing:0;padding:0 .563rem;border:1px solid #9daca9;border-radius:4px;background-color:#fff;box-shadow:inset 0 1px rgba(0,0,0,0.1);color:#454545;vertical-align:top}.topcoat-text-input:focus,.topcoat-text-input--large:focus{background-color:#fff;color:#454545;border:1px solid #0036ff;box-shadow:0 0 0 2px #6fb5f1}.topcoat-text-input:disabled::-webkit-input-placeholder{color:#454545}.topcoat-text-input:disabled::-moz-placeholder{color:#454545}.topcoat-text-input:disabled:-ms-input-placeholder{color:#454545}.topcoat-text-input:invalid{border:1px solid #ec514e}.topcoat-text-input--large{line-height:1.688rem;font-size:.875rem}.topcoat-text-input--large:disabled{color:#454545}.topcoat-text-input--large:disabled::-webkit-input-placeholder{color:#454545}.topcoat-text-input--large:disabled::-moz-placeholder{color:#454545}.topcoat-text-input--large:disabled:-ms-input-placeholder{color:#454545}.topcoat-text-input--large:invalid{border:1px solid #ec514e}.topcoat-textarea,.topcoat-textarea--large{padding:1rem;font-size:1rem;font-weight:400;border-radius:4px;line-height:1.313rem;border:1px solid #9daca9;background-color:#fff;box-shadow:inset 0 1px rgba(0,0,0,0.1);color:#454545;letter-spacing:0}.topcoat-textarea:focus,.topcoat-textarea--large:focus{background-color:#fff;color:#454545;border:1px solid #0036ff;box-shadow:0 0 0 2px #6fb5f1}.topcoat-textarea:disabled::-webkit-input-placeholder{color:#454545}.topcoat-textarea:disabled::-moz-placeholder{color:#454545}.topcoat-textarea:disabled:-ms-input-placeholder{color:#454545}.topcoat-button,.topcoat-button--quiet,.topcoat-button--large,.topcoat-button--large--quiet,.topcoat-button--cta,.topcoat-button--large--cta{padding:0 .563rem;font-size:12px;line-height:1.313rem;letter-spacing:0;color:#454545;text-shadow:0 1px #fff;vertical-align:top;background-color:#e5e9e8;box-shadow:inset 0 1px #fff;border:1px solid #9daca9;border-radius:4px}.topcoat-button:hover,.topcoat-button--quiet:hover,.topcoat-button--large:hover,.topcoat-button--large--quiet:hover{background-color:#eff1f1}.topcoat-button:focus,.topcoat-button--quiet:focus,.topcoat-button--quiet:hover:focus,.topcoat-button--large:focus,.topcoat-button--large--quiet:focus,.topcoat-button--large--quiet:hover:focus,.topcoat-button--cta:focus,.topcoat-button--large--cta:focus{border:1px solid #0036ff;box-shadow:inset 0 1px rgba(255,255,255,0.36),0 0 0 2px #6fb5f1;outline:0}.topcoat-button:active,.topcoat-button--large:active{border:1px solid #9daca9;background-color:#d2d6d6;box-shadow:inset 0 1px rgba(0,0,0,0.1)}.topcoat-button--large,.topcoat-button--large--quiet{font-size:.875rem;font-weight:600;line-height:1.688rem;padding:0 .875rem}.topcoat-button--large--cta{font-size:.875rem;font-weight:600;line-height:1.688rem;padding:0 .875rem}.topcoat-button--cta,.topcoat-button--large--cta{border:1px solid #134f7f;background-color:#288edf;box-shadow:inset 0 1px rgba(255,255,255,0.36);color:#fff;font-weight:500;text-shadow:0 -1px rgba(0,0,0,0.36)}.topcoat-button--cta:hover,.topcoat-button--large--cta:hover{background-color:#4ca1e4}.topcoat-button--cta:active,.topcoat-button--large--cta:active{background-color:#1e7dc8;box-shadow:inset 0 1px rgba(0,0,0,0.12)}
+            *{box-sizing:border-box}html{background:#F4F4F4}body{font-size:large;padding:1em}body,input,textarea{color:#797B7B;font-family:"Helvetica-Neue",Helvetica,sans-serif}input,textarea{width:100%}input[type=submit]{width:auto}h1{font-family: 'Bangers', cursive;margin-top:0;}.form-item{margin-bottom:1em}.topcoat-text-input,.topcoat-text-input--large{line-height:1.313rem;font-size:12px;letter-spacing:0;padding:0 .563rem;border:1px solid #9daca9;border-radius:4px;background-color:#fff;box-shadow:inset 0 1px rgba(0,0,0,0.1);color:#454545;vertical-align:top}.topcoat-text-input:focus,.topcoat-text-input--large:focus{background-color:#fff;color:#454545;border:1px solid #0036ff;box-shadow:0 0 0 2px #6fb5f1}.topcoat-text-input:disabled::-webkit-input-placeholder{color:#454545}.topcoat-text-input:disabled::-moz-placeholder{color:#454545}.topcoat-text-input:disabled:-ms-input-placeholder{color:#454545}.topcoat-text-input:invalid{border:1px solid #ec514e}.topcoat-text-input--large{line-height:1.688rem;font-size:.875rem}.topcoat-text-input--large:disabled{color:#454545}.topcoat-text-input--large:disabled::-webkit-input-placeholder{color:#454545}.topcoat-text-input--large:disabled::-moz-placeholder{color:#454545}.topcoat-text-input--large:disabled:-ms-input-placeholder{color:#454545}.topcoat-text-input--large:invalid{border:1px solid #ec514e}.topcoat-textarea,.topcoat-textarea--large{padding:1rem;font-size:1rem;font-weight:400;border-radius:4px;line-height:1.313rem;border:1px solid #9daca9;background-color:#fff;box-shadow:inset 0 1px rgba(0,0,0,0.1);color:#454545;letter-spacing:0}.topcoat-textarea:focus,.topcoat-textarea--large:focus{background-color:#fff;color:#454545;border:1px solid #0036ff;box-shadow:0 0 0 2px #6fb5f1}.topcoat-textarea:disabled::-webkit-input-placeholder{color:#454545}.topcoat-textarea:disabled::-moz-placeholder{color:#454545}.topcoat-textarea:disabled:-ms-input-placeholder{color:#454545}.topcoat-button,.topcoat-button--quiet,.topcoat-button--large,.topcoat-button--large--quiet,.topcoat-button--cta,.topcoat-button--large--cta{padding:0 .563rem;font-size:12px;line-height:1.313rem;letter-spacing:0;color:#454545;text-shadow:0 1px #fff;vertical-align:top;background-color:#e5e9e8;box-shadow:inset 0 1px #fff;border:1px solid #9daca9;border-radius:4px}.topcoat-button:hover,.topcoat-button--quiet:hover,.topcoat-button--large:hover,.topcoat-button--large--quiet:hover{background-color:#eff1f1}.topcoat-button:focus,.topcoat-button--quiet:focus,.topcoat-button--quiet:hover:focus,.topcoat-button--large:focus,.topcoat-button--large--quiet:focus,.topcoat-button--large--quiet:hover:focus,.topcoat-button--cta:focus,.topcoat-button--large--cta:focus{border:1px solid #0036ff;box-shadow:inset 0 1px rgba(255,255,255,0.36),0 0 0 2px #6fb5f1;outline:0}.topcoat-button:active,.topcoat-button--large:active{border:1px solid #9daca9;background-color:#d2d6d6;box-shadow:inset 0 1px rgba(0,0,0,0.1)}.topcoat-button--large,.topcoat-button--large--quiet{font-size:.875rem;font-weight:600;line-height:1.688rem;padding:0 .875rem}.topcoat-button--large--cta{font-size:.875rem;font-weight:600;line-height:1.688rem;padding:0 .875rem}.topcoat-button--cta,.topcoat-button--large--cta{border:1px solid #134f7f;background-color:#288edf;box-shadow:inset 0 1px rgba(255,255,255,0.36);color:#fff;font-weight:500;text-shadow:0 -1px rgba(0,0,0,0.36)}.topcoat-button--cta:hover,.topcoat-button--large--cta:hover{background-color:#4ca1e4}.topcoat-button--cta:active,.topcoat-button--large--cta:active{background-color:#1e7dc8;box-shadow:inset 0 1px rgba(0,0,0,0.12)}
         </style>
     </head>
     <body>
-        <h1>The Nitch Podcast "Sode-O-Matic!"</h1>
+        <h1>The Nitch Podcast "'Sode&#8209;O&#8209;Matic!"</h1>
         <form method="post" accept-charset="utf-8">
+            <div class="form-item" id="publication_date_wrapper">
+                <label for="publication_date">Publication Date</label><br>
+                <input class="topcoat-text-input--large" type="date" id="publication_date" name="publication_date" value="<?php echo $this_friday ?>" placeholder="e.g., 2013-11-29" required>
+            </div>
             <div class="form-item" id="episode_number_wrapper">
                 <label for="episode_number">Episode Number</label><br>
-                <input class="topcoat-text-input--large" type="text" id="episode_number" name="episode_number" value="" placeholder="e.g., 84" required autofocus>
+                <input class="topcoat-text-input--large" type="text" id="episode_number" name="episode_number" value="<?php echo $this_episode_number ?>" placeholder="e.g., 84" required>
             </div>
             <div class="form-item" id="episode_name_wrapper">
                 <label for="episode_name">Episode Name</label><br>
-                <input class="topcoat-text-input--large" type="text" id="episode_name" name="episode_name" value="" placeholder="e.g., Pizza Car" required>
+                <input class="topcoat-text-input--large" type="text" id="episode_name" name="episode_name" value="" placeholder="e.g., Pizza Car" required autofocus>
             </div>
             <div class="form-item" id="duration_wrapper">
                 <label for="duration">Duration</label><br>
@@ -144,13 +173,9 @@ if (isset($_POST['action'])) {
                 <label for="file_size">File Size (in megabytes)</label><br>
                 <input class="topcoat-text-input--large" type="text" id="file_size" name="file_size" value="" placeholder="e.g., 47.3" required>
             </div>
-            <div class="form-item" id="publication_date_wrapper">
-                <label for="publication_date">Publication Date</label><br>
-                <input class="topcoat-text-input--large" type="date" id="publication_date" name="publication_date" value="" placeholder="e.g., 2013-11-29" required>
-            </div>
             <div class="form-item" id="description_wrapper">
                 <label for="description">Description</label><br>
-                <textarea class="topcoat-textarea" rows="2" id="description" name="description" placeholder="e.g., Jonathan and Kelli talk about yada yada yada..." required></textarea>
+                <textarea class="topcoat-textarea" rows="2" id="description" name="description" placeholder="e.g., Jonathan and Kelli talk about yada yada yada..." required>Jonathan and Kelli talk about </textarea>
             </div>
             <div class="form-item" id="housekeeping_wrapper">
                 <label for="housekeeping">Housekeeping</label><br>
