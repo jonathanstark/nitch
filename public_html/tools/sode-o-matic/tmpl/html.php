947c726f4343a8d82adce7f31593e8bbf89ab7b1
Jonathan Stark <jstark@jonathanstark.com>
2013-12-10 17:02:57 -0500

    Added js to preview the audio file and episode display name so i can cut and paste into Audacity export dialog

--- public_html/tools/sode-o-matic/tmpl/html.php
+++ public_html/tools/sode-o-matic/tmpl/html.php
@@ -9,6 +9,8 @@
     <body>
         <h1>The Nitch Podcast "'Sode&#8209;O&#8209;Matic!"</h1>
         <div id="script">
+            <h3 id="display_name_preview">Episode XX: Foo Bar Baz!</h3>
+            <p id="audio_filename_preview">Episode_XX_Foo_Bar_Baz.mp3</p>
             <p>Hello! And welcome to the Nitch Podcast for <?php echo date('l, F jS, Y', strtotime($this_friday)) ?>. I'm JS (I'm KS) and we're here to talk about building apps that run everywhere using open web standards like HTML, CSS, JavaScript, REST, and JSON.</p>
             <p>This week! We discuss <span id="description_preview"></span></p>
             <p>Please stay tuned, the Nitch Podcast is next...</p>
@@ -49,10 +51,23 @@
             <input class="topcoat-button--large--cta" type="submit" name="action" value="Allons-y!">
         </form>
         <script>
-            // Update the script as description is typed
-            document.getElementById('description').addEventListener('keyup', function() {
-                document.getElementById('description_preview').innerText = this.value;
-            }, false);
+            function updateDescriptionPreview() {
+                document.getElementById('description_preview').innerText = document.getElementById('description').value;
+            }
+            function updateNamePreview() {
+                var audio_filename, display_name;
+                display_name = 'Episode ' + document.getElementById('episode_number').value + ': ' + document.getElementById('episode_name').value;
+                audio_filename = display_name.replace(/[^a-zA-Z0-9 ]+/g, ''); // Remove non-alphanum
+                audio_filename = audio_filename.replace(/^\s+|\s+$/g, ''); // Trim whitespace
+                audio_filename = audio_filename.replace(/ /g, '_'); // Replace whitespace
+                audio_filename+= '.mp3';
+                document.getElementById('display_name_preview').innerText = display_name;
+                document.getElementById('audio_filename_preview').innerText = audio_filename;
+            }
+
+            // Update the previews as they are typed
+            document.getElementById('description').addEventListener('keyup', updateDescriptionPreview, false);
+            document.getElementById('episode_name').addEventListener('keyup', updateNamePreview, false);
 
             // Loop through inputs
             var watched_ids = ['description', 'duration', 'episode_name', 'housekeeping', 'links', 'titles'];
@@ -68,7 +83,10 @@
                 if (!!localStorage.getItem(id)) {
                     document.getElementById(id).value = localStorage.getItem(id);
                     if (id == 'description') {
-                        document.getElementById('description_preview').innerText = localStorage.getItem(id);
+                        updateDescriptionPreview();
+                    }
+                    if (id == 'episode_name') {
+                        updateNamePreview();
                     }
                 }
 
